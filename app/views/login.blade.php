191b670b114e8a5b43b0568feae70d2ef1293992
maticv <matic.volk@gmail.com>
2014-07-29 13:16:09 +0200

    db config

--- app/views/login.blade.php
+++ app/views/login.blade.php
@@ -5,9 +5,32 @@
 @stop
 
 @section('content')
-	{{ Former::open('login')->method('post') }}
+<form class="form-horizontal">
+  <div class="control-group">
+    <label class="control-label" for="inputEmail">Email</label>
+    <div class="controls">
+      <input type="text" id="inputEmail" placeholder="Email">
+    </div>
+  </div>
+  <div class="control-group">
+    <label class="control-label" for="inputPassword">Password</label>
+    <div class="controls">
+      <input type="password" id="inputPassword" placeholder="Password">
+    </div>
+  </div>
+  <div class="control-group">
+    <div class="controls">
+      <label class="checkbox">
+        <input type="checkbox"> Remember me
+      </label>
+      <button type="submit" class="btn">Sign in</button>
+    </div>
+  </div>
+</form>
+	<!--{{ Former::open('login')->method('post') }}
     {{ Former::text('email','Email:')->autofocus() }}
     {{ Former::password('password','Password:') }}
     {{ Former::submit('login','Login') }}
-    {{ Former::close() }}
+    {{ Former::close() }}-->
 @stop
+
